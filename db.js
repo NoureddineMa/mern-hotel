96fa1a5972f5ee20e65fa58b03b00e7befcb9f1e
NoureddineMa <noureddinemaher13@gmail.com>
2023-05-30 17:50:00 +0200

    update

---
+++ db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+var mongoDBURL = 'mongodb+srv://maher:maher@cluster0.thzdxhy.mongodb.net/mern-rooms'
+
+mongoose.connect(mongoDBURL , {useUnifiedTopology:true , useNewUrlParser:true})
+
+var dbconnect = mongoose.connection
+
+dbconnect.on('error' , ()=>{
+    console.log(`Mongo DB Connection Failed`);
+})
+
+dbconnect.on('connected' , ()=>{
+    console.log(`Mongo DB Connection Successfull`);
+})
+
+module.exports = mongoose
